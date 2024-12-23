9e8196808451aba11f574535d035c841184eb775
Соломин Павел <solomin_pasha_cool@mail.ru>
2024-12-23 14:38:59 +0300

    Протестированная версия, непонятная ошибка в ~65-67 строке при запуске кода без комментариев

--- topsize.sh
+++ topsize.sh
@@ -3,26 +3,34 @@ args=($@)
 i=0
 k=0
 opt_d=0
-opt_h=0
 opt_N=0
 opt_s=0
 opt=0
+st=1
 h=""
-minsize="1b"
-while [[ opt == 0 ]] && [[ $i < ${#args[*]} ]]; do
+minsize=1b
+while [[ $opt == 0 ]] && [[ $i < ${#args[*]} ]]; do
+        z=${args[$(($i+1))]}
         if [[ ${args[$i]} == "-h" ]]; then
-                opt_h=1
                 h="-h"
         elif [[ ${args[$i]} =~ ^-[0-9]+$ ]]; then
                 N=${args[$i]}
+                opt_N=1
         elif [[ ${args[$i]} == "-s" ]]; then
-                min_size=$((${args[$(($i+1))]}-1))
-                opt_s=1
+                z=${args[$(($i+1))]}
+                echo z $z
+                u=${z:0:$((${#z}-1))}
+                echo u $u
+                if [[  ${z:(-1) =~ [bkMG] ]]; then
+                        minsize=$(($u-1))${z:(-1)}
+                        echo minsize $minsize
+                        opt_s=1
+                else
+                       echo "invalid argument for [-s minsize]"
+                fi
         elif [[ ${args[$i]} == "--" ]]; then
                 opt=1
                 st=$(($i+1))
-        elif  [[ ${args[$i]} == "/*" ]]; then
-                opt_d=1
         elif [[ ${args[$i]} == "--help" ]]; then
                         echo "topsize [--help][-h][-N][-s minsize][--][dir...]"
                         echo "  "
@@ -35,50 +43,70 @@ while [[ opt == 0 ]] && [[ $i < ${#args[*]} ]]; do
                         echo "   -N     quontity of files (all files if not stated)"
                         echo "   -s minsize     the required size to be displayed by this script (1b if not stated)"
                         exit 0
-        elif [[ ${files[$i]} == -* ]] && [[ ${files[$i]} != "-h" ]] && [[ ${files[$i]} != "-s" ]] && [[ ${files[$i]} != "/*" ]]; then
-                        echo "Option ${files[$i]} is not supported" 
+        elif [[ ${args[$i]} != -* ]] && [[ ${args[$i]} != "-h" ]] && [[ ${args[$i]} != "-s" ]] && [[ ! ${args[$i]} =~ ^-[0-9]+$ ]] && [[ ${args[$(($i-1))]} != "-s" ]]; then
+                        echo "Option ${args[$i]} is not supported" 
                         exit 1
-	fi
+        else
+                if [[ ! -d ${args[$i]} ]]; then
+                        echo "Directory ${args[$i]} doesn't exist or path to it is icomplete"
+                fi
+        fi
         i=$(($i+1))
 done
-
-if [[ opt_N == 1 ]]; then
-        for (( i=0; i<$st; i++ )); do
-                if  [[ ${args[$i]} == "/*" ]]; then
-                        y=$(find ${args[$i]} -type f -size +$minsize -exec du $h {} + | sort -r | head $N)
-                        k=$(($k+1))
-                        echo $y
+if [[ $st > 1 ]]; then
+        for (( i=$st; i < ${#args[*]}; i++ )); do
+                if [[ ! -d ${args[$i]} ]]; then
+                        echo "Directory ${args[$i]} doesn't exist or path to it is incomplete"
+                        exit 1
                 fi
         done
-        for (( i=$st; i<${#args[*]}; i++ )); do
-                if  [[ ${args[$i]} == "/*" ]]; then
-                        y=$(find ${args[$i]} -type f -size +$minsize -exec du $h {} + | sort -r | head $N)
-                        k=$(($k+1))
-                        echo $y
-                fi
-        done
-        if [[ k == 0 ]]; then
-                y=$(find . -type f -size +$minsize -exec du -h {} + | sort -r | head $N)
-                echo $y
+fi
+if [[ ! ${args[0]} =~ -* ]] && [[ $opt_N == 1 ]]; then
+        find ${args[0]} -type f -size +$minsize -exec du $h {} +  | sort -r | head $N
+elif [[ ! ${args[0]} =~ -* ]] && [[ $opt_N == 0 ]]; then
+         find ${args[0]} -type f -size +$minsize -exec du $h {} +  | sort -r
+fi
+for (( i=1; i<$st; i++ )); do
+        echo "Предыдущий ${args[$(($i-1))]}"
+        if  [[ ! ${args[$i]} =~ -* ]] && [[ $opt_N == 1 ]] && [[ ${args[$(($i-1))]} != "-s" ]]; then
+                find ${args[$i]} -type f -size +$minsize -exec du $h {} + | sort -r | head $N
+                echo 1 "find ${args[$i]} -type f -size +$minsize -exec du $h {} + | sort -r | head $N"
+                k=$(($k+1))
+        elif [[ ! ${args[$i]} =~ -* ]] && [[ $opt_N == 0 ]] && [[ ${args[$(($i-1))]} != "-s" ]]; then
+                find ${args[$i]} -type f -size +$minsize -exec du $h {} + | sort -r
+                echo 2 "find ${args[$i]} -type f -size +$minsize -exec du $h {} + | sort -r" 
+                k=$(($k+1))
         fi
-elif [[ opt_N == 0 ]]; then
-        for (( i=0; i<$st; i++ )); do
-                if  [[ ${args[$i]} == "/*" ]]; then
-                        y=$(find ${args[$i]} -type f -size +$minsize -exec du $h {} + | sort -r)
+done
+for (( i=$st; i<${#args[*]}; i++ )); do
+        if [[ $st > 1 ]]; then
+                if  [[ $opt_N == 1 ]]; then
+                        find ${args[$i]} -type f -size +$minsize -exec du $h {} + | sort -r | head $N
+                        echo 3 "find ${args[$i]} -type f -size +$minsize -exec du $h {} + | sort -r | head $N"
                         k=$(($k+1))
-                        echo $y
-                fi
-        done
-        for (( i=$st; i<${#args[*]}; i++ )); do
-                if  [[ ${args[$i]} == "/*" ]]; then
-                        y=$(find ${args[$i]} -type f -size +$minsize -exec du $h {} + | sort -r)
+                else
+                        echo 4 "find ${args[$i]} -type f -size +$minsize -exec du $h {} + | sort -r" 
+                        find ${args[$i]} -type f -size +$minsize -exec du $h {} + | sort -r
                         k=$(($k+1))
-                        echo $y
                 fi
-        done
-        if [[ k == 0 ]]; then
-                y=$(find . -type f -size +$minsize -exec du -h {} + | sort -r)
-                echo $y
+        else
+                if  [[ ${args[$i]} != -* ]] && [[ $opt_N == 1 ]] && [[ ${args[$(($i-1))]} != "-s" ]]; then
+                        find ${args[$i]} -type f -size +$minsize -exec du $h {} + | sort -r | head $N
+                        echo 5 "find ${args[$i]} -type f -size +$minsize -exec du $h {} + | sort -r | head $N"
+                        k=$(($k+1))
+                elif [[ ${args[$i]} != -* ]] && [[ $opt_N == 0 ]] && [[ ${args[$(($i-1))]} != "-s" ]]; then
+                        find ${args[$i]} -type f -size +$minsize -exec du $h {} + | sort -r
+                        echo 6 "find ${args[$i]} -type f -size +$minsize -exec du $h {} + | sort -r" 
+                        k=$(($k+1))
+                fi
+        fi
+done
+if [[ $k == 0 ]]; then
+        if [[ $opt_N == 1 ]]; then
+                find . -type f -size +$minsize -exec du $h {} + | sort -r | head $N
+                echo 7 "find . -type f -size +$minsize -exec du $h {} + | sort -r | head $N"
+        else
+                find . -type f -size +$minsize -exec du $h {} + | sort -r
+                echo 8 "find . -type f -size +$minsize -exec du $h {} + | sort -r" 
         fi
 fi
-
